b6dc60751cc46b5c9c37811c946697f3ab4f5ebf
Roman <shukailo99@gmail.com>
2021-03-15 21:45:55 +0300

    no message

---
+++ Notes/Notes.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Notes {
+    var titleOfNote : String
+    let id : String
+}
+
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -1,19 +1,49 @@
-//
-//  ViewController.swift
-//  Notes
-//
-//  Created by Roman Shukailo on 15.03.21.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var firstTableView = UITableView(frame: .zero, style: .plain)
+    var identifier = "MyCell"
+    var notes = [Notes(titleOfNote: "Помыть машину", id : UUID().uuidString)]
+                
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+        self.title = "Ваши заметки"
+        createTable()
+        view.addSubview(firstTableView)
     }
-
-
+    
+    func createTable() {
+        self.firstTableView.frame = view.bounds
+        firstTableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        self.firstTableView.delegate = self
+        self.firstTableView.dataSource = self
+        firstTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(firstTableView)
+    }
+    //MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        let note = notes[indexPath.row]
+        
+        cell.textLabel?.text = notes.titleOfNote
+        
+        return cell
+    }
+    
+    //MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
 }
+
+
+
 
